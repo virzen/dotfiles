cc787bfe21652afab59094d46cd477d78fc828a5
Virzen <wiktor.czajkowski@gmail.com>
2016-03-25 02:54:50 +0100

    Add install script
    Script pulls the latest version of dotfiles from the git repo and symlinks files to the user's home directory

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+export DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# update .dotfiles repo first
+[ -d "$DOTFILES_DIR"/.git ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
+
+# symlinks
+# s - soft link
+# v - verbose
+# f - force (overwrite existing files)
+ln -svf "$DOTFILES_DIR/.bash_aliases" ~
+ln -svf "$DOTFILES_DIR/.bashrc" ~
+ln -svf "$DOTFILES_DIR/.profile" ~
+ln -svf "$DOTFILES_DIR/.bash_logout" ~
+ln -svf "$DOTFILES_DIR/.gitconfig" ~
+# ln -svf "$DOTFILES_DIR/git/.gitignore_global" ~
