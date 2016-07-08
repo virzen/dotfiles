6c96bf73fa295ec78d506077242504d9ae9b671f
Wiktor Czajkowski <wiktor.czajkowski@netguru.co>
2016-07-08 20:29:33 +0200

    simplify install script

--- install.sh
+++ install.sh
@@ -9,13 +9,6 @@ export DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # v - verbose
 # f - force (overwrite existing files)
 ln -svf "$DOTFILES_DIR/.bash_aliases" ~
-ln -svf "$DOTFILES_DIR/.bashrc" ~
-ln -svf "$DOTFILES_DIR/.profile" ~
-ln -svf "$DOTFILES_DIR/.bash_logout" ~
-ln -svf "$DOTFILES_DIR/.gitconfig" ~
-ln -svf "$DOTFILES_DIR/.vimrc" ~
-ln -svf "$DOTFILES_DIR/.vim" ~
-# ln -svf "$DOTFILES_DIR/git/.gitignore_global" ~
 
 # install nvm
-wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+# wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
