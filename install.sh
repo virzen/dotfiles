ceef424276e742b5286bd4b9370035c2253f4312
Wiktor Czajkowski <wiktor.czajkowski@netguru.co>
2016-07-08 20:36:07 +0200

    link zsh config from install script

--- install.sh
+++ install.sh
@@ -4,11 +4,17 @@ export DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # update .dotfiles repo first
 [ -d "$DOTFILES_DIR"/.git ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 
+# install zsh
+
+# make zsh default shell
+# chsh -s $(which zsh)
+
 # symlinks
 # s - soft link
 # v - verbose
 # f - force (overwrite existing files)
 ln -svf "$DOTFILES_DIR/.bash_aliases" ~
+ln -svf "$DOTFILES_DIR/.zshrc" ~
 
 # install nvm
 # wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
