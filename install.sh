55a70042e361fa29253651f89fc04c23197cf909
Wiktor Czajkowski <wiktor.czajkowski@gmail.com>
2016-12-04 22:32:20 +0100

    :art: Move dotfiles repo update

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 export DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# update .dotfiles repo first
-[ -d "$DOTFILES_DIR"/.git ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
-
 #=== Helpers ===#
 command_exists() {
   command -v "$1" >/dev/null 2>&1 || { echo >&2 "$1 not installed."; exit 1; }
@@ -11,5 +8,7 @@ command_exists() {
 
 ln -svf "$DOTFILES_DIR/.zshrc" ~
 ln -svf "$DOTFILES_DIR/.gitconfig" ~
+#=== Dotfiles repo update ===#
+[ -d "$DOTFILES_DIR"/.git ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 
 # wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
