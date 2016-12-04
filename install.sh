d6e7103ec3e525b9e4220b213ff9c0950a78d6c3
Wiktor Czajkowski <wiktor.czajkowski@gmail.com>
2016-12-04 22:32:51 +0100

    :art: Move and add title to symlinks section

--- install.sh
+++ install.sh
@@ -6,9 +6,11 @@ command_exists() {
   command -v "$1" >/dev/null 2>&1 || { echo >&2 "$1 not installed."; exit 1; }
 }
 
-ln -svf "$DOTFILES_DIR/.zshrc" ~
-ln -svf "$DOTFILES_DIR/.gitconfig" ~
 #=== Dotfiles repo update ===#
 [ -d "$DOTFILES_DIR"/.git ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 
 # wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+
+#=== Symlinks ===#
+ln -svf "$DOTFILES_DIR/.zshrc" ~
+ln -svf "$DOTFILES_DIR/.gitconfig" ~
