3c828814aef646c55e0325addfb80f429bdcf20a
Wiktor Czajkowski <wiktor.czajkowski@gmail.com>
2016-12-06 13:11:24 +0100

    :wrench: Add alias for pushing with tracking to origin

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -40,7 +40,9 @@ alias gm='git merge'
 alias gms='git merge --squash'
 
 alias gpl='git pull'
+
 alias gps='git push'
+alias gpsuo='git push -u origin'
 alias gpsnv='git push --no-verify'
 
 #=== terminal window ===#
