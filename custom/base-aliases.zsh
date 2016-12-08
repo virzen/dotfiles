168d9df7ba8fc894a6fe166a51c3cdd069b4d350
Wiktor Czajkowski <wiktor.czajkowski@gmail.com>
2016-12-09 00:26:44 +0100

    :fire: Remove comments form base aliases

--- custom/base-aliases.zsh
+++ custom/base-aliases.zsh
@@ -1,22 +1,15 @@
-#=== editors ===#
 alias a='atom'
 alias v='vim'
 
-#=== config files ===#
 alias vimrc='vim ~/.vimrc'
 
-#=== terminal window ===#
-alias cl='clear'
 alias restart='exec zsh'
 
-#=== browser sync ===#
 bs() {
   browser-sync start --server --files '**' --no-online --no-open $@ &
 }
 
-#=== rails ===#
 alias rserv='bundle exec rails server'
 alias rmg='rails db:migrate RAILS_ENV=development'
 
-#=== ssh ===#
 alias pubkey='cat ~/.ssh/id_rsa.pub | pbcopy && echo "Public key copied to clipboard!"'
