fcccd9a484b8521d50597257ac862bdd8926f05e
Wiktor Czajkowski <wiktor.czajkowski@gmail.com>
2017-01-26 18:27:08 +0100

    Add heading to sections

--- custom/base-aliases.zsh
+++ custom/base-aliases.zsh
@@ -1,28 +1,40 @@
+#=== Configs ===#
 alias vimrc='vim ~/.vimrc'
 alias zshrc='vim ~/.zshrc'
 
+
+#=== Aliases ===#
 # FIXME
 alias bals='vim ~/Workspace/dotfiles/custom/base-aliases.zsh'
 alias gals='vim ~/Workspace/dotfiles/custom/git-aliases.zsh'
 
+
+#=== Shell ===#
 alias restart='exec zsh'
 alias :q='exit'
 
+
+#=== File watching ===#
 fsw() {
   fswatch $2 | xargs -n 1 $1
 }
 alias fswpy='fsw python'
 alias fswpy3='fsw python3'
 
-alias testping='ping 8.8.8.8'
-alias wifi_re='sudo ifconfig en0 down && sudo ifconfig en0 up'
-
 bs() {
   browser-sync start --server --files '**' --no-online --no-open $@ &
 }
 
+
+#=== Network ===#
+alias testping='ping 8.8.8.8'
+alias wifi_re='sudo ifconfig en0 down && sudo ifconfig en0 up'
+
+
+#=== SSH ===#
 alias pubkey='cat ~/.ssh/id_rsa.pub | pbcopy && echo "Public key copied to clipboard!"'
 
+#=== RoR ===#
 alias fstart='foreman start -f Procfile.dev'
 
 alias rserv='bundle exec rails server'
