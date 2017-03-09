611044deeeeba7a6d09dd5b00f19a745bad25d89
Wiktor Czajkowski <wiktor.czajkowski@gmail.com>
2017-03-09 02:37:01 +0100

    Use quotes for paths
    In case there are some spaces in them

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ if [[ -d "$HOME/homebrew/.git" ]]; then
 else
   heading "Installing homebrew"
   echo "Cloning homebrew to $HOME/hombrew..."
-  git clone git@github.com:Homebrew/brew.git $HOME/homebrew
+  git clone git@github.com:Homebrew/brew.git "$HOME/homebrew"
   if [[ $? -eq 0 ]]; then
     echo "Brew installation successful. Make sure you have \$HOME/homebrew/bin in your PATH."
   fi
@@ -28,23 +28,27 @@ fi
 # zsh -> oh my zsh
 
 heading "Creating symlinks"
-ln -svf "$DOTFILES_DIR/.zprofile" $HOME
-ln -svf "$DOTFILES_DIR/.zshrc" $HOME
-ln -svf "$DOTFILES_DIR/.gitconfig" $HOME
-ln -svf "$DOTFILES_DIR/.vimrc" $HOME
-ln -svf "$DOTFILES_DIR/.editorconfig" $HOME
-ln -svf "$DOTFILES_DIR/.tmux.conf" $HOME
-ln -svf "$DOTFILES_DIR/.agignore" $HOME
-ln -svf "$DOTFILES_DIR/.npmrc" $HOME
-mkdir -v $HOME/.config/
-mkdir -v $HOME/.config/karabiner/
-ln -svf "$DOTFILES_DIR/karabiner.json" $HOME/.config/karabiner/
+ln -svf "$DOTFILES_DIR/.zprofile"     "$HOME"
+ln -svf "$DOTFILES_DIR/.zshrc"        "$HOME"
+ln -svf "$DOTFILES_DIR/.gitconfig"    "$HOME"
+ln -svf "$DOTFILES_DIR/.vimrc"        "$HOME"
+ln -svf "$DOTFILES_DIR/.editorconfig" "$HOME"
+ln -svf "$DOTFILES_DIR/.tmux.conf"    "$HOME"
+ln -svf "$DOTFILES_DIR/.agignore"     "$HOME"
+ln -svf "$DOTFILES_DIR/.npmrc"        "$HOME"
+
+if [[ ! -d $HOME/.config/karabiner ]]; then
+  md -v "$HOME/.config/karabiner/"
+fi
+ln -svf "$DOTFILES_DIR/karabiner.json" "$HOME/.config/karabiner/"
+
+heading "Copying stuff"
 cp -v "$DOTFILES_DIR/com.googlecode.iterm2.plist" $HOME
 
 heading "zsh-nvm"
-if [[ -d $HOME/.oh-my-zsh/custom/plugins/zsh-nvm ]]; then
+if [[ -d "$HOME/.oh-my-zsh/custom/plugins/zsh-nvm" ]]; then
   echo "Existing installation detected. Removing..."
-  rm -rf $HOME/.oh-my-zsh/custom/plugins/zsh-nvm
+  rm -rf "$HOME/.oh-my-zsh/custom/plugins/zsh-nvm"
 fi
 echo "Installing fresh one..."
-git clone https://github.com/lukechilds/zsh-nvm $HOME/.oh-my-zsh/custom/plugins/zsh-nvm
+git clone https://github.com/lukechilds/zsh-nvm "$HOME/.oh-my-zsh/custom/plugins/zsh-nvm"
