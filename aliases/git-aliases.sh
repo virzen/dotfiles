a28f417880d1b354c666fc49f525b0798e9a245c
Wiktor Czajkowski <wiktor.czajkowski@gmail.com>
2017-09-13 14:22:07 +0200

    Add prettier git log alias

--- aliases/git-aliases.sh
+++ aliases/git-aliases.sh
@@ -22,7 +22,8 @@ alias grh='git reset --hard'
 
 alias gu='git undo'
 
-alias gl='git log --oneline'
+alias gl="git log --graph --pretty=format:'%Cred%h%Creset
+-%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
 alias glm='git log --oneline --author="Wiktor Czajkowski"'
 alias gll='git log'
 
