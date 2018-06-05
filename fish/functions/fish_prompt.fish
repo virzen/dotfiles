812aabe279e156643cd79dc8da44230bf30e62f4
Wiktor Czajkowski <wiktor.czajkowski@gmail.com>
2018-06-06 00:45:20 +0200

    Add fish configuration
    Fish is fucking awesome, where has it been all my life.

---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,13 @@
+function fish_prompt --description 'Write out the prompt'
+	if not set -q __fish_prompt_normal
+        set -g __fish_prompt_normal (set_color normal)
+    end
+
+    if not set -q __fish_prompt_cwd
+        set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+    end
+
+    echo ''
+    echo -s "$USER" ' ' "$__fish_prompt_cwd" (prompt_pwd) (__fish_vcs_prompt) "$__fish_prompt_normal" 
+    echo '> '
+end
