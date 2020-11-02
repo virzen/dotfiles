3ba3850e8560f1e4e5716adecd007edfe2e2ed5d
Wiktor Czajkowski <wiktor.czajkowski@gmail.com>
2020-11-02 16:58:55 +0100

    backup before giving away the laptop

---
+++ mac/fish/config.fish
@@ -0,0 +1,5 @@
+eval (direnv hook fish)
+
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/wiktorczajkowski/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/wiktorczajkowski/Downloads/google-cloud-sdk/path.fish.inc'; end
